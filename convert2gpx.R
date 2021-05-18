f77b937e40afc8ce72b5b05b3afaed7f0fc714d4
Colin Chisholm <colin.chisholm@unbc.ca>
2021-05-18 09:13:31 -0700

    added GPX extensions

--- convert2gpx.R
+++ convert2gpx.R
@@ -8,9 +8,10 @@ library(rgdal)
 
 
 ## Make these first variables inputs of function to wrap all below
-input <- "d:/GIS/ALRF/Projects/2020/BlkDev/blk8/SelCutSurvey.gpkg"
-NameField <- "name"
-filename <- "output"
+input <- "d:/GIS/ALRF/Projects/2021/StandingTimberInventory/SW_STI_Recce_Plots.gpkg"
+t <- st_read(input)
+NameField <- "plot"
+filename <- "SW_STI_Recce_Plots"
 setwd("d:/GIS/GIS GPS/")
 
 
@@ -22,13 +23,19 @@ setwd("d:/GIS/GIS GPS/")
 ###     re-opened using rgdal.  
 c_write_OGR <- function (sfdat, layer, filename){
   
+  ## testing
+  # sfdat <- dat
+  # layer <- "waypoints"
+  
+  
   st_write(sfdat, "tmp.gpkg",   ## write out the sf object (temp file) 
            delete_dsn = TRUE)   
   readOGR("tmp.gpkg")           ## open with gdal  
-  dat <- readOGR("e:/tmp.gpkg") 
+  dat <- readOGR("tmp.gpkg") 
   writeOGR(dat, paste0(filename, ".gpx"),  ## export GPX 
            driver = "GPX", 
            layer = layer,       ## options: waypoints, routes, tracks 
+           dataset_options = "GPX_USE_EXTENSIONS=yes",
            overwrite_layer = TRUE,
            delete_dsn = TRUE)   ## will delete the output if it exists! 
   unlink("tmp.gpkg")
@@ -49,7 +56,8 @@ write_GPX <- function (input, NameField, output) {
   ##  read the data and determine type 
   dat <- st_read(input) %>% 
     st_transform(.,4326) %>% ## tranform to WGS 84 for GPS use
-    dplyr::rename(name = name)
+    dplyr::rename(Name = NameField) %>% 
+    mutate(Name = as.character(Name))
     
   ## Type of geometry
   gclass <- st_geometry_type(dat) %>% unique(.) %>% as.character(.)
@@ -60,7 +68,7 @@ write_GPX <- function (input, NameField, output) {
   ## a. open via sf and convert to point
     dat <- dat %>% st_cast(., "POINT")
     
-    ## save sf, open with gdal and save with gdal --- _make this a function_
+    ## calls function above: save sf, open with gdal and save with gdal
     c_write_OGR(dat, "waypoints", filename)
   
     
@@ -87,6 +95,10 @@ write_GPX <- function (input, NameField, output) {
 write_GPX(input, NameField, filename)
   
  
+
+#### DRAFT: Save Polygon as line -------------
+
+
 
 
 
